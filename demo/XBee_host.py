ed85b50a6536a85168e6236658e1673e951ec3f8
ChenFangYi <bonebony1231@gmail.com>
2020-05-26 04:07:13 +0800

    xx

---
+++ demo/XBee_host.py
@@ -0,0 +1,55 @@
+import serial
+import time
+
+# XBee setting
+serdev = '/dev/ttyUSB2'
+s = serial.Serial(serdev, 9600)
+
+s.write("+++".encode())
+char = s.read(2)
+print("Enter AT mode.")
+print(char.decode())
+
+s.write("ATMY 0x140\r\n".encode())
+char = s.read(3)
+print("Set MY 0x140.")
+print(char.decode())
+
+s.write("ATDL 0x240\r\n".encode())
+char = s.read(3)
+print("Set DL 0x240.")
+print(char.decode())
+
+s.write("ATID 0x1\r\n".encode())
+char = s.read(3)
+print("Set PAN ID 0x1.")
+print(char.decode())
+
+s.write("ATWR\r\n".encode())
+char = s.read(3)
+print("Write config.")
+print(char.decode())
+
+s.write("ATMY\r\n".encode())
+char = s.read(4)
+print("MY :")
+print(char.decode())
+
+s.write("ATDL\r\n".encode())
+char = s.read(4)
+print("DL : ")
+print(char.decode())
+
+s.write("ATCN\r\n".encode())
+char = s.read(3)
+print("Exit AT mode.")
+print(char.decode())
+
+print("start sending ACC")
+while True:
+    # send acc to remote
+    s.write("/ACC/run 0 0\r\n".encode())
+    time.sleep(1)
+
+
+s.close()
